f5b45c3666f2e8e967496fbf8bfcf26806277d63
Avery Kyle Arenas <kyruken@gmail.com>
2022-09-23 21:26:24 -0700

    add home module

---
+++ src/home.js
@@ -0,0 +1,23 @@
+import Icon from './image0.jpeg';
+
+export default function header() {
+    const header = document.createElement('header');
+
+    const restaurantName = document.createElement('h1');
+    const restaurantType = document.createElement('h2');
+    const description = document.createElement('p');
+    const headerImage = new Image();
+
+    restaurantName.textContent = `Ibraheem's Taqueria`;
+    restaurantType.textContent = 'Indian and Mexican Fusion';
+    description.textContent = `Coming straight from the bay is Ibraheem's halal mexican food!`;
+    headerImage.src = Icon;
+
+    header.appendChild(restaurantName);
+    header.appendChild(restaurantType);
+    header.appendChild(description);
+    header.appendChild(headerImage);
+
+    return header;
+
+}
--- src/index.js
+++ src/index.js
@@ -1,29 +1,8 @@
-import Icon from './image0.jpeg';
-function header() {
-    const header = document.createElement('header');
-
-    const restaurantName = document.createElement('h1');
-    const restaurantType = document.createElement('h2');
-    const description = document.createElement('p');
-    const headerImage = new Image();
-
-    restaurantName.textContent = `Ibraheem's Taqueria`;
-    restaurantType.textContent = 'Indian and Mexican Fusion';
-    description.textContent = `Coming straight from the bay is Ibraheem's halal mexican food!`;
-    headerImage.src = Icon;
-
-    header.appendChild(restaurantName);
-    header.appendChild(restaurantType);
-    header.appendChild(description);
-    header.appendChild(headerImage);
-
-    return header;
-
-}
+import home from './home.js';
 
 function initHomePage() {
-    const root = document.getElementById('content');
-
+    const content = document.getElementById("content");
+    document.body.appendChild(home());
 }
 
-document.body.appendChild(header());
+initHomePage();

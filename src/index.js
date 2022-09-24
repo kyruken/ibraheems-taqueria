7a32d57aba15db8159e016e330a3ae0ff88d4a14
Avery Kyle Arenas <kyruken@gmail.com>
2022-09-23 20:12:12 -0700

    first javascript element append

---
+++ src/index.js
@@ -0,0 +1,8 @@
+function headerText() {
+    const header1 = document.createElement('h1');
+    header1.textContent = "Ibraheem's Taqueria";
+
+    return header1;
+}
+
+document.body.appendChild(headerText());

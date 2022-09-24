587bea9e84795a1b9517da1bbcbfbc7c7ec77168
Avery Kyle Arenas <kyruken@gmail.com>
2022-09-23 20:39:45 -0700

    add header elements

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
-function headerText() {
-    const header1 = document.createElement('h1');
-    header1.textContent = "Ibraheem's Taqueria";
+import Icon from './image0.jpeg';
+function header() {
+    const header = document.createElement('header');
 
-    return header1;
+    const restaurantName = document.createElement('h1');
+    const restaurantType = document.createElement('h2');
+    const description = document.createElement('p');
+    const headerImage = new Image();
+
+    restaurantName.textContent = `Ibraheem's Taqueria`;
+    restaurantType.textContent = 'Indian and Mexican Fusion';
+    description.textContent = `Coming straight from the bay is Ibraheem's halal mexican food!`;
+    headerImage.src = Icon;
+
+    header.appendChild(restaurantName);
+    header.appendChild(restaurantType);
+    header.appendChild(description);
+    header.appendChild(headerImage);
+
+    return header;
+
 }
 
-document.body.appendChild(headerText());
+function initHomePage() {
+    const root = document.getElementById('content');
+
+}
+
+document.body.appendChild(header());
